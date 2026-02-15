3b666607b71f5e12deeaf090321580628818a389
Nivas-git07 <itsmenivas007@gmail.com>
2026-02-15 17:19:10 +0000

    add event description

---
+++ src/components/eventcomponent/eventdetails.js
@@ -0,0 +1,68 @@
+export default function EventDetails() {
+  return (
+    <section className="eventDescSection">
+
+      <div className="eventDescContainer">
+
+        {/* ===== Banner ===== */}
+        <div className="eventBanner">
+
+          <span className="bannerBar leftBar"></span>
+          <span className="bannerBar rightBar"></span>
+
+          <p className="eventDate">AUGUST 5 - 8, 2025</p>
+
+          <h1 className="eventTitle">
+            TRY SUMMER CHAMPIONSHIPS
+          </h1>
+
+        </div>
+
+       
+        <h3 className="eventSubTitle">DETAILS</h3>
+
+        <div className="eventDetailsGrid">
+
+          <div className="detailRow">
+            <span>Organized By</span>
+            <p>Malaysia Finswimming Association</p>
+          </div>
+
+          <div className="detailRow">
+            <span>Date</span>
+            <p>August 5 – 8, 2025</p>
+          </div>
+
+          <div className="detailRow">
+            <span>Venue</span>
+            <p>Malaysia , Kuala Lumpur</p>
+          </div>
+
+          <div className="detailRow">
+            <span>Time</span>
+            <p>09:00 AM</p>
+          </div>
+
+        </div>
+
+      
+        <h3 className="eventSubTitle">DESCRIPTION</h3>
+
+        <p className="eventDescription">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula,
+          lorem a porttitor porttitor, velit erat tincidunt lorem, in pulvinar justo
+          turpis vitae eros. Curabitur nec nisi in ipsum dignissim lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula,
+          lorem a porttitor porttitor, velit erat tincidunt lorem, in pulvinar justo
+          turpis vitae eros. Curabitur nec nisi in ipsum dignissim lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula,
+          lorem a porttitor porttitor, velit erat tincidunt lorem, in pulvinar justo
+          turpis vitae eros. Curabitur nec nisi in ipsum dignissim lacinia.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula,
+          lorem a porttitor porttitor, velit erat tincidunt lorem, in pulvinar justo
+          turpis vitae eros. Curabitur nec nisi in ipsum dignissim lacinia.
+
+        </p>
+
+      </div>
+
+    </section>
+  )
+}
--- src/route/route.js
+++ src/route/route.js
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../page/login/login";
 import Home from "../page/home/home";
 import Event from "../page/event/event";
+import Eventview from "../page/eventdescription/eventview";
 function Page() {
     return (
         <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Home />} />
-            <Route path="/event" element={<Event/>} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/eventview" element={<Eventview />} />
         </Routes>
     )
 }
