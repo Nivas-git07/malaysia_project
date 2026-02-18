9bc3a872b7434136fa4036c7a24c5683bdb5833d
Nivas-git07 <itsmenivas007@gmail.com>
2026-02-18 18:49:59 +0000

    add register api

--- src/user/api/auth.js
+++ src/user/api/auth.js
@@ -1,4 +1,4 @@
 import API from "./axiosmodal";
 
-export const athelete_register = (govt_id, email_id, full_name, phone_number, role, gender, date_of_birth, password) =>
-    API.post("/athlette_register", { govt_id, email_id, full_name, phone_number, role, gender, date_of_birth, password })
+export const athelete_register = (full_name, govt_id, email_id, phone_number, role = "ATHELETE", gender, date_of_birth, state, password) =>
+    API.post("/athlette_register", { govt_id, email_id, full_name, phone_number, role, gender, date_of_birth, state, password })
--- src/user/auth/form/atheleform.js
+++ src/user/auth/form/atheleform.js
@@ -1,25 +1,58 @@
+import { useState } from "react"
+import { athelete_register } from "../../api/auth"
+
+
 function Atheleform() {
+    const [name, setname] = useState("");
+    const [govt_id, setgovt_id] = useState("");
+    const [email, setemail] = useState("");
+    const [phonenumber, setphonenumber] = useState("");
+    const [gender, setgender] = useState("");
+    const [dob, setdob] = useState("");
+    const [password, setpassword] = useState("");
+    const [state, setstate] = useState("");
+
+
+
+    const handleclick = async () => {
+        try {
+            const response = athelete_register(name, govt_id, email, phonenumber, gender, dob, state, password);
+            console.log(response.data);
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <form className="regForm">
+        <form className="regForm" onSubmit={handleclick()}>
 
             <div className="regGridLabel">
-            <p className="regRowLabel">Athlete</p>
-            <p className="regRowLabel">Govt-id</p>
+                <p className="regRowLabel">Athlete</p>
+                <p className="regRowLabel">Govt-id</p>
             </div>
             <div className="regGrid">
-                <input className="regInput" placeholder="First Name" />
-                <input className="regInput" placeholder="Govt-id" />
+                <input className="regInput" placeholder="First Name" value={name} onChange={(e) => {
+                    setname(e.target.value)
+                }} />
+                <input className="regInput" placeholder="Govt-id" value={govt_id} onChange={(e) => {
+                    setgovt_id(e.target.value)
+                }} />
             </div>
 
-       
+
             <div className="regGridLabel">
                 <p className="regRowLabel">Email</p>
                 <p className="regRowLabel">Mobile Number</p>
             </div>
 
             <div className="regGrid">
-                <input className="regInput" placeholder="e.g.,example@email.com" />
-                <input className="regInput" placeholder="e.g., +60 12 345 678" />
+                <input className="regInput" placeholder="e.g.,example@email.com" value={email} onChange={(e) => {
+                    setemail(e.target.value)
+                }} />
+                <input className="regInput" placeholder="e.g., +60 12 345 678" value={phonenumber} onChange={(e) => {
+                    setphonenumber(e.target.value)
+                }} />
             </div>
 
             <div className="regGridLabel">
@@ -28,10 +61,13 @@ function Atheleform() {
             </div>
 
             <div className="regGrid">
-                <input type="date" className="regInput" />
+                <input type="date" className="regInput" value={dob} onChange={(e) => {
+                    setdob(e.target.value)
+                }} />
 
                 <div className="regField">
-                    <select className="regSelect">
+                    <select className="regSelect" value={gender}
+                        onChange={(e) => setgender(e.target.value)}>
                         <option value="">Select Gender</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
@@ -41,9 +77,16 @@ function Atheleform() {
 
             </div>
 
-    
+
             <p className="regRowLabel">State / Region</p>
-            <input className="regInput full" placeholder="State / Region" />
+            <input className="regInput full" placeholder="State / Region" value={state} onChange={(e) => setstate(e.target.value)
+
+            } />
+            <p className="regRowLabel">Password</p>
+            <input className="regInput full" placeholder="* * * * * * * *" value={password} onChange={(e) => setpassword(e.target.value)
+
+            } />
+
 
             <button className="regBtn">Register</button>
 
