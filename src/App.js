cd37c21e1e5316cbbf350cd0ebfaf38b99166fc3
aakash2820 <aakashsaravanan2007@gmail.com>
2026-02-19 21:43:58 +0530

    commited profile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import AdminLogin from "./admin/page/login/AdminLogin";
+import AdminRoute from "./admin/route/route";
 import Page from "./user/route/route";
-
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return(
   <Router>
+    <AdminRoute />
     <Page />
-    <AdminLogin />
+
   </Router>
   )
 }
--- src/admin/route/route.js
+++ src/admin/route/route.js
@@ -1,10 +1,12 @@
 import AdminLogin from "../page/login/AdminLogin";
 import { Routes, Route } from "react-router-dom";
+import ProfileForm from "../page/profile_page/ProfileForm";
 
 function AdminRoute() {
     return (
         <Routes>
         <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/profile" element={<ProfileForm />} />
     </Routes>
     )
 }
