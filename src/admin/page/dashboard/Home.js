7626ff6f31114e816fdb369d1f50240158aaa695
Nivas-git07 <itsmenivas007@gmail.com>
2026-03-12 19:31:50 +0000

    change layout position

--- src/admin/page/dashboard/Home.js
+++ src/admin/page/dashboard/Home.js
@@ -17,79 +17,81 @@ export default function Home() {
   const navigate = useNavigate();
   console.log(data?.data.stats);
   return (
-    
+
     <>
       <Navbar />
+      <div className="mu-membership-wrapper">
 
-     
-      <div className="dataTitle">DATA OVERVIEW</div>
 
-      
-      <div className="overviewCards">
-        <div className="overviewCard">
-          <p>Total States</p>
-          <h2>{data?.data.stats.total_states || 0}</h2>
-        </div>
+        <div className="dataTitle">DATA OVERVIEW</div>
 
-        <div className="overviewCard">
-          <p>Total Clubs</p>
-          <h2>{data?.data.stats.total_clubs || 0}</h2>
-        </div>
 
-        <div className="overviewCard">
-          <p>Total Members</p>
-          <h2>{data?.data.stats.total_athletes || 0} +</h2>
-        </div>
-      </div>
-
-      
-      <div className="stateTitle">STATE LIST</div>
-
-     
-      <div className="stateCard">
-        
-        <div className="stateFilters">
-          <input placeholder="e.g., Selangor Finswimming Club" />
-          <input placeholder="--Select State--" />
-          <button className="findBtn">Find Club</button>
-        </div>
-
-     
-        <div className="stateTable">
-          <div className="stateHead">
-            <div>State Name</div>
-            <div>Members</div>
-            <div>Club Count</div>
-            <div>Website</div>
+        <div className="overviewCards">
+          <div className="overviewCard">
+            <p>Total States</p>
+            <h2>{data?.data.stats.total_states || 0}</h2>
           </div>
 
-          {data?.data.states_list?.map((club, i) => (
-            <div className="stateRow" key={i} onClick={() => navigate(`/admin/home/state/${club.id}`)}>
-              <div className="clubCell">
-                <img src={logo} alt="logo" />
+          <div className="overviewCard">
+            <p>Total Clubs</p>
+            <h2>{data?.data.stats.total_clubs || 0}</h2>
+          </div>
 
-                {data?.data.states_list[i].state_name}
-              </div>
+          <div className="overviewCard">
+            <p>Total Members</p>
+            <h2>{data?.data.stats.total_athletes || 0} +</h2>
+          </div>
+        </div>
 
-              <div className="membersCell">
-                <img src="https://i.pravatar.cc/40" />
-                <img src="https://i.pravatar.cc/41" />
-                <img src="https://i.pravatar.cc/42" />
-                <span> + {data?.data.states_list[i].members_count} </span>
-              </div>
 
-              <div>{data?.data.states_list[i].clubs_count}</div>
+        <div className="stateTitle">STATE LIST</div>
 
-              <a
-                href="https://www.georgetown.com"
-                className="website"
-                target="_blank"
-                rel="noreferrer"
-              >
-                www.georgetown.com
-              </a>
+
+        <div className="stateCard">
+
+          <div className="stateFilters">
+            <input placeholder="e.g., Selangor Finswimming Club" />
+            <input placeholder="--Select State--" />
+            <button className="findBtn">Find Club</button>
+          </div>
+
+
+          <div className="stateTable">
+            <div className="stateHead">
+              <div>State Name</div>
+              <div>Members</div>
+              <div>Club Count</div>
+              <div>Website</div>
             </div>
-          ))}
+
+            {data?.data.states_list?.map((club, i) => (
+              <div className="stateRow" key={i} onClick={() => navigate(`/admin/home/state/${club.id}`)}>
+                <div className="clubCell">
+                  <img src={logo} alt="logo" />
+
+                  {data?.data.states_list[i].state_name}
+                </div>
+
+                <div className="membersCell">
+                  <img src="https://i.pravatar.cc/40" />
+                  <img src="https://i.pravatar.cc/41" />
+                  <img src="https://i.pravatar.cc/42" />
+                  <span> + {data?.data.states_list[i].members_count} </span>
+                </div>
+
+                <div>{data?.data.states_list[i].clubs_count}</div>
+
+                <a
+                  href="https://www.georgetown.com"
+                  className="website"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  www.georgetown.com
+                </a>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
