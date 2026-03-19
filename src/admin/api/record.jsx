d919a72711ad10304ba611ab9a36c8e2bbb65d2b
Nivas-git07 <itsmenivas007@gmail.com>
2026-03-19 10:38:58 +0000

    complete get enent name and athlete records

--- src/admin/api/record.jsx
+++ src/admin/api/record.jsx
@@ -1,5 +1,9 @@
 import API from "../../user/api/axiosmodal";
 
 export const geteventRecords = () => {
-    return API.get("/super/fetch_tickets/");
-}
+  return API.get("/super/fetch_tickets/");
+};
+
+export const get_athlete_records = () => {
+  return API.get("/super/get_athlete_list/?page=athlete_record");
+};
