9ab773b15acdc15cb6fc36a9703d834c340f6b83
Nivas-git07 <itsmenivas007@gmail.com>
2026-03-24 18:45:13 +0000

    add navigation in nav bar

--- src/user/layout/navbar.jsx
+++ src/user/layout/navbar.jsx
@@ -22,27 +22,47 @@ function Navbar() {
   return (
     <nav className="mfsaPro-navbar">
       <div className="mfsaPro-container">
-       
         <ul className="mfsaPro-menu mfsaPro-left">
-          <li>HOME</li>
-          <li>ABOUT US</li>
+          <li
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            HOME
+          </li>
+          <li
+            onClick={() => {
+              navigate("/about");
+            }}
+          >
+            ABOUT US
+          </li>
           <li>MEMBERSHIP</li>
-          <li>ASSOCIATIONS</li>
+          <li
+            onClick={() => {
+              navigate("/association");
+            }}
+          >
+            ASSOCIATIONS
+          </li>
         </ul>
 
-        
         <div className="mfsaPro-logo">
           <img src={logo} alt="logo" />
         </div>
 
-        
         <ul className="mfsaPro-menu mfsaPro-left">
           <li>EVENTS</li>
           <li>NEWS</li>
-          <li>CONTACT</li>
+          <li
+            onClick={() => {
+              navigate("/contact");
+            }}
+          >
+            CONTACT
+          </li>
         </ul>
 
-    
         <div className="mfsaPro-user">
           <img src="https://static.vecteezy.com/system/resources/previews/009/734/564/non_2x/default-avatar-profile-icon-of-social-media-user-vector.jpg" />
           <span>Nivas</span>
