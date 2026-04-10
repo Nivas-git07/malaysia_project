8b65d33f945c625b121b3b671bd6fcf53d86cae9
Nivas <Nivas-git07>
2026-04-10 16:50:26 +0000

    add transition effect on user site

--- src/user/route/route.jsx
+++ src/user/route/route.jsx
@@ -1,5 +1,10 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import ScrollToTop from "../hooks/scrolltotop";
+import PageWrapper from "../hooks/animateroute";
+
+// pages
 import Register from "../page/register/register";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "../page/login/login";
 import Home from "../page/home/home";
 import Event from "../page/event/event";
@@ -8,7 +13,6 @@ import About from "../page/about/about";
 import AllAthelete from "../page/allatheletes/athelete";
 import Association from "../page/association/association";
 import Athelete from "../page/athelete/athelteprofile";
-import ScrollToTop from "../hooks/scrolltotop";
 import StatePage from "../page/state/state";
 import ClubPage from "../page/club/club";
 import Contact from "../page/contact/contact";
@@ -19,57 +23,62 @@ import Membership from "../page/membership/membership";
 import Gallery from "../page/gallery/gallery";
 import Registermembershipsubmission from "../page/register/registermemsubmission";
 import Aboutpreview from "../../admin/previewtemplate/aboutpreview";
-function Page() {
+
+export default function Page() {
+  const location = useLocation();
+
   return (
     <>
       <ScrollToTop />
-      <Routes>
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="" element={<Home />} />
-        <Route path="state/:stateId" element={<StatePage />} />
-        <Route path="state/:stateId/club/:clubId" element={<ClubPage />} />
-        <Route path="event" element={<Event />} />
-        <Route path="state/:stateId/club/:clubId/event" element={<Event />} />
-        <Route path="state/:stateId/event" element={<Event />} />
-        <Route path="eventview/:eventId" element={<Eventview />} />
-        <Route path="about" element={<About />} />
-        <Route path="state/:stateId/club/:clubId/about" element={<About />} />
-        <Route path="state/:stateId/about" element={<About />} />
-        <Route
-          path="state/:stateId/club/:clubId/athletes"
-          element={<AllAthelete />}
-        />
-        <Route path="/athletes" element={<AllAthelete />} />
 
-        <Route path="association" element={<Association />} />
-        <Route path="state/:stateId/association" element={<Association />} />
-        <Route path="athelete" element={<Athelete />} />
-        <Route path="contact" element={<Contact />} />
-        <Route
-          path="state/:stateId/club/:clubId/contact"
-          element={<Contact />}
-        />
-        <Route path="news" element={<News />} />
-        <Route path="/state/:stateId/news" element={<News />} />
-        <Route path="/state/:stateId/club/:clubId/news" element={<News />} />
-        <Route path="registerabout" element={<RegistrationAboutForm />} />
-        <Route path="registermembership" element={<Registermembership />} />
-        <Route path="membershipabout" element={<Membership />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="/state/:stateId/gallery" element={<Gallery />} />
-        <Route
-          path="/state/:stateId/club/:clubId/gallery"
-          element={<Gallery />}
-        />
-        <Route
-          path="membershipsubmission"
-          element={<Registermembershipsubmission />}
-        />
-        <Route path="aboutpreview" element={Aboutpreview} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+
+          <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+          <Route path="register" element={<PageWrapper><Register /></PageWrapper>} />
+          <Route path="login" element={<PageWrapper><Login /></PageWrapper>} />
+
+          <Route path="state/:stateId" element={<PageWrapper><StatePage /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId" element={<PageWrapper><ClubPage /></PageWrapper>} />
+
+          <Route path="event" element={<PageWrapper><Event /></PageWrapper>} />
+          <Route path="state/:stateId/event" element={<PageWrapper><Event /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId/event" element={<PageWrapper><Event /></PageWrapper>} />
+
+          <Route path="eventview/:eventId" element={<PageWrapper><Eventview /></PageWrapper>} />
+
+          <Route path="about" element={<PageWrapper><About /></PageWrapper>} />
+          <Route path="state/:stateId/about" element={<PageWrapper><About /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId/about" element={<PageWrapper><About /></PageWrapper>} />
+
+          <Route path="/athletes" element={<PageWrapper><AllAthelete /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId/athletes" element={<PageWrapper><AllAthelete /></PageWrapper>} />
+
+          <Route path="association" element={<PageWrapper><Association /></PageWrapper>} />
+          <Route path="state/:stateId/association" element={<PageWrapper><Association /></PageWrapper>} />
+
+          <Route path="athelete" element={<PageWrapper><Athelete /></PageWrapper>} />
+
+          <Route path="contact" element={<PageWrapper><Contact /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId/contact" element={<PageWrapper><Contact /></PageWrapper>} />
+
+          <Route path="news" element={<PageWrapper><News /></PageWrapper>} />
+          <Route path="state/:stateId/news" element={<PageWrapper><News /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId/news" element={<PageWrapper><News /></PageWrapper>} />
+
+          <Route path="gallery" element={<PageWrapper><Gallery /></PageWrapper>} />
+          <Route path="state/:stateId/gallery" element={<PageWrapper><Gallery /></PageWrapper>} />
+          <Route path="state/:stateId/club/:clubId/gallery" element={<PageWrapper><Gallery /></PageWrapper>} />
+
+          <Route path="registerabout" element={<PageWrapper><RegistrationAboutForm /></PageWrapper>} />
+          <Route path="registermembership" element={<PageWrapper><Registermembership /></PageWrapper>} />
+          <Route path="membershipabout" element={<PageWrapper><Membership /></PageWrapper>} />
+          <Route path="membershipsubmission" element={<PageWrapper><Registermembershipsubmission /></PageWrapper>} />
+
+          <Route path="aboutpreview" element={<PageWrapper><Aboutpreview /></PageWrapper>} />
+
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
-
-export default Page;
